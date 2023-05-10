136902c6506f0c3a0fc26f4d3a34f911389eaadb
Andy N <andynguyen9001@gmail.com>
2023-05-09 21:45:25 -0600

    Add functionality to hamburger menu

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".hamburger-menu");
+const hamburgerMenu = document.querySelector(".hamburger-menu-items");
+const hamburgerMenuCloseButton = document.querySelector("#hamburger-menu-close-button");
+
+hamburger.addEventListener("click", () => {
+    hamburgerMenu.style.display = "block";
+});
+
+hamburgerMenuCloseButton.addEventListener("click", () => {
+    hamburgerMenu.style.display = "none";
+})
+
